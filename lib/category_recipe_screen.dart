fa1e88d911eba2853bba43a20375dc1885043862
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2020-02-12 15:38:44 +0530

    RecipeScreen

---
+++ lib/category_recipe_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryRecipeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("The Recipes"),),
+          body: Center(
+        child: Text(
+          "Some Random Text!!",
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,20 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-          body1: TextStyle(color:Color.fromRGBO(20, 51, 51, 1),),
-          body2: TextStyle(color:Color.fromRGBO(20, 51, 51, 1),),
-          title: TextStyle(fontSize: 24.0, fontFamily: 'RobotoCondensed'),
-        ),
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
   }
 }
-
-
