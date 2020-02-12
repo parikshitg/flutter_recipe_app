52adb56970ca19dd86592aad7519bf2680f20ddb
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2020-02-12 16:17:09 +0530

    some changes id '

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 import './category_recipe_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryRecipeScreen();
+          return CategoryRecipeScreen(id, title);
         },
       ),
     );
--- lib/category_recipe_screen.dart
+++ lib/category_recipe_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryRecipeScreen extends StatelessWidget {
+
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryRecipeScreen(this.categoryId,this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("The Recipes"),),
+      appBar: AppBar(title: Text(categoryTitle),),
           body: Center(
         child: Text(
           "Some Random Text!!",
