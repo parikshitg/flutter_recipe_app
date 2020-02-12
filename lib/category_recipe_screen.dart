bf464775e34f353884e4d50e687228396bcc6ac4
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2020-02-12 17:24:20 +0530

    printing recipestitle in categoriesscreen

--- lib/category_recipe_screen.dart
+++ lib/category_recipe_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/models/recipe.dart';
+
+import './dummy_data.dart';
 
 class CategoryRecipeScreen extends StatelessWidget {
 
@@ -11,14 +14,16 @@ class CategoryRecipeScreen extends StatelessWidget {
 
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryRecipes = DUMMY_RECIPES.where((recipe) {
+      return recipe.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle),),
-          body: Center(
-        child: Text(
-          "Some Random Text!!",
-        ),
-      ),
+          body: ListView.builder(itemBuilder: (ctx, index){
+            return Text(categoryRecipes[index].title);
+          },
+          itemCount: categoryRecipes.length,),
     );
   }
 }
